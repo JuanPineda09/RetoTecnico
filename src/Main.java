4d23644e6bbac73db593f0263e5f87e596426827
JuanPineda <juanpineda420@gmail.com>
2022-07-13 21:26:22 -0500

    Se crean carpetas Fronted y Backend para comenzar con el Fronted

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        Conexiones cc = new Conexiones();
+        Connection cn = cc.conectar();
+       /* Scanner sc = new Scanner(System.in);
+        String response = String.valueOf(sc.nextLine());
+        System.out.println(response);*/
     }
 }
